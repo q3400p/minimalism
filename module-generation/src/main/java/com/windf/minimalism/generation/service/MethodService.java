fed7332ab6d8a6ceeb0f43b8d712d6b6409661df
windfChen <q3400p@sina.cn>
2020-01-11 23:04:23 +0800

    bugfix:方法服务，继承错误修改

--- module-generation/src/main/java/com/windf/minimalism/generation/service/MethodService.java
+++ module-generation/src/main/java/com/windf/minimalism/generation/service/MethodService.java
@@ -2,6 +2,7 @@ package com.windf.minimalism.generation.service;
 
 import com.windf.core.service.ManageService;
 import com.windf.minimalism.generation.entity.Entity;
+import com.windf.minimalism.generation.entity.Method;
 
-public interface MethodService extends ManageService<Entity> {
+public interface MethodService extends ManageService<Method> {
 }
