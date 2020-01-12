441903a845477d2f844f354ef03dc1e9c82ebf30
windfChen <q3400p@sina.cn>
2020-01-12 19:59:20 +0800

    重构：去除无效引用

--- module-generation/src/main/java/com/windf/minimalism/generation/entity/type/BooleanType.java
+++ module-generation/src/main/java/com/windf/minimalism/generation/entity/type/BooleanType.java
@@ -1,7 +1,5 @@
 package com.windf.minimalism.generation.entity.type;
 
-import com.windf.minimalism.generation.entity.Type;
-
 public class BooleanType extends BaseType {
     @Override
     public LangType getLangType() {
