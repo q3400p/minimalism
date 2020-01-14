91c4154fc175f742340d64bd2bd3b5f9db5926e8
chenyafeng <chenyafeng@whaty.com>
2020-01-14 20:13:27 +0800

    功能：日期工具类补充

--- core/src/main/java/com/windf/core/util/DateUtil.java
+++ core/src/main/java/com/windf/core/util/DateUtil.java
@@ -19,33 +19,6 @@ public class DateUtil {
     public static final String[] FULL_DATE_TIME_Arr = new String[]{FULL_DATE_TIME, SIMPLE_DATE, SMART_DATE, FULL_TIME, YEAR_DATE, SIMPLE_TIME, SMART_TIME};
     public static final String[] SIMPLE_DATE_ARR = new String[]{SIMPLE_DATE};
     
-    public static final Integer NINETYSECONDS = 90; //90 秒
-    public static final Integer THREEMINS = 3 * 60; //3分钟（秒）
-    public static final Integer FULLDAY = 60 * 60 * 24; //24小时 (秒)
-    public static final Integer ONEHOUR = 60 * 60; //1小时 (秒)
-    public static final Integer EXPIRTMILLIS = 30 * 60; //30分钟
-    public static final Integer SMALLEXPIRTMILLIS = 10 * 60; //10分钟
-    public static final Integer RESENDEMAILTIME = 5 * 60; //可重复发送邮件的周期 5分钟
-    public static final Integer WEEKTIMES = 7 * 24 * 60 * 60; //一周 （秒）
-    public static final Integer REMEMBERCOOKIES = Integer.MAX_VALUE;
-    public static final long TENMINUTES = 600000; //10分钟（毫秒）
-    
-    /**
-     * 校验是否为合法日期格式
-     * @param str
-     * @param type
-     * @return
-     */
-    public static boolean isValidDate(String str, String[] type) {
-        boolean convertSuccess=true;
-        try {
-            DateUtils.parseDate(str, type);
-        } catch (ParseException e) {
-            convertSuccess=false;
-        }
-        return convertSuccess;
-    }
-    
     /**
      * 解析时间字符串为时间格式
      * @param dateStr
@@ -85,5 +58,44 @@ public class DateUtil {
     	}
     	
 		return result;
+    }
+
+    /**
+     * 按照默认格式解析当前时间
+     * yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String format() {
+        return format(new Date(), FULL_DATE_TIME);
+    }
+
+    /**
+     * 按照指定格式解析当前时间
+     * yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String format(String format) {
+        return format(new Date(), format);
+    }
+
+    /**
+     * 按照默认格式解析时间
+     * yyyy-MM-dd HH:mm:ss
+     * @param date
+     * @return
+     */
+    public static String format(Date date) {
+        return format(date, FULL_DATE_TIME);
+    }
+
+    /**
+     * 按指定格式解析时间
+     * @param date
+     * @param format
+     * @return
+     */
+    public static String format(Date date, String format) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        return simpleDateFormat.format(date);
     }
 }
