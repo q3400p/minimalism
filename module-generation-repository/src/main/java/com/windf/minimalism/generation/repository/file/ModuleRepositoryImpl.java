55e0f34bce91bcd1d4f87c70f94947c2ea0b1c7a
chenyafeng <chenyafeng@whaty.com>
2020-01-16 15:15:51 +0800

    功能：初始化模块存储，导入文件存储插件

---
+++ module-generation-repository/src/main/java/com/windf/minimalism/generation/repository/file/ModuleRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.windf.minimalism.generation.repository.file;
+
+import com.windf.minimalism.generation.entity.Module;
+import com.windf.plugin.repository.file.BaseManageRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ModuleRepositoryImpl extends BaseManageRepository<Module> {
+
+}
