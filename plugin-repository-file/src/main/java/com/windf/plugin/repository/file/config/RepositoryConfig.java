61581ed8fbbc5a2fa3cdcf10b5e4c39b68008ef0
chenyafeng <chenyafeng@whaty.com>
2020-01-17 19:26:53 +0800

    功能：修改文件保存路径

--- plugin-repository-file/src/main/java/com/windf/plugin/repository/file/config/RepositoryConfig.java
+++ plugin-repository-file/src/main/java/com/windf/plugin/repository/file/config/RepositoryConfig.java
@@ -4,7 +4,7 @@ public class RepositoryConfig {
     /**
      * 用于存储的路径
      */
-    private String homePath = System.getProperty("user.home");
+    private String homePath = System.getProperty("user.home") + "/.windf";
 
     public String getHomePath() {
         return homePath;
