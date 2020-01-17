b108eba4e4bdbadd693287f21df7a3806d0daa71
chenyafeng <chenyafeng@whaty.com>
2020-01-17 19:11:49 +0800

    功能：重新设置home路径

--- plugin-repository-file/src/main/java/com/windf/plugin/repository/file/config/RepositoryConfig.java
+++ plugin-repository-file/src/main/java/com/windf/plugin/repository/file/config/RepositoryConfig.java
@@ -4,7 +4,7 @@ public class RepositoryConfig {
     /**
      * 用于存储的路径
      */
-    private String homePath = "~";
+    private String homePath = System.getProperty("user.home");
 
     public String getHomePath() {
         return homePath;
