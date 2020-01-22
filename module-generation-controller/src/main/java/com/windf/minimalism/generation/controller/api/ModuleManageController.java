3e43c7686439d883d3d3b40f927e31ebb68d6bac
windfChen <q3400p@sina.cn>
2020-01-22 19:21:30 +0800

    重构：json读写功能抽取新类BaseJSONFileRepository

--- module-generation-controller/src/main/java/com/windf/minimalism/generation/controller/api/ModuleManageController.java
+++ module-generation-controller/src/main/java/com/windf/minimalism/generation/controller/api/ModuleManageController.java
@@ -1,9 +1,8 @@
 package com.windf.minimalism.generation.controller.api;
 
-import com.windf.core.entity.ResultData;
-import com.windf.plugin.controller.api.controller.BaseManageController;
 import com.windf.minimalism.generation.entity.Module;
 import com.windf.minimalism.generation.service.ModuleService;
+import com.windf.plugin.controller.api.controller.BaseManageController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
