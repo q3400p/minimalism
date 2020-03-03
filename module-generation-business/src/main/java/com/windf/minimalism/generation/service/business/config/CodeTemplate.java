abda297f556b8ee4a71e895bf20b687eeee34f29
windfChen <q3400p@sina.cn>
2020-03-03 11:48:00 +0800

    提交目录修改

--- module-generation-business/src/main/java/com/windf/minimalism/generation/service/business/config/CodeTemplate.java
+++ module-generation-business/src/main/java/com/windf/minimalism/generation/service/business/config/CodeTemplate.java
@@ -6,7 +6,7 @@ public class CodeTemplate {
      * 代码的生成位置
      */
 //    private String targetPath = System.getProperty("user.home") + "/.windf/target";
-    private String targetPath = "/Development/workspace/work/jkkc_java";
+    private String targetPath = "/Development/workspace/work/jkkc2020/server";
 
     /**
      * 定义文件的结尾符号
